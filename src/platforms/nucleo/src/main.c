0e358aa18144733c8f622eed0181d8aef76c01ff
Alan Thiessen <AlanDThiessen@gmail.com>
2019-01-18 21:25:05 -0500

    Fix Nucleo HAL.

--- src/platforms/nucleo/src/main.c
+++ src/platforms/nucleo/src/main.c
@@ -10,10 +10,23 @@
 
 
 #include "stm32f4xx.h"
-			
+#include "stm32f4xx_nucleo.h"
 
 int main(void)
 {
+   // Initialize the Hardware Abstraction Library
+   HAL_Init();
 
-	for(;;);
+   // Initialize LED2 on the NUCLEO board
+   BSP_LED_Init(LED2);
+
+   // Loop forever blinking LED2
+   for(;;)
+   {
+       // Toggle LED2
+       BSP_LED_Toggle(LED2);
+
+       // Wait for 500 milliseconds
+       HAL_Delay(500);
+   }
 }
